7f12d005f2ae4f598e89155915bac23d13b6afb6
pxjoke <pxjoke@gmail.com>
2015-10-26 19:00:31 +0300

    Module 5 task 02  added

---
+++ src/com/epam/module5/task02/PropertiesReader.java
@@ -0,0 +1,19 @@
+package com.epam.module5.task02;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by pxjok on 26.10.2015.
+ */
+public class PropertiesReader {
+    private Map<String, String> properties = new HashMap<>();
+
+    public void load(String fileName) throws FileNotFoundException, IOException{
+
+    }
+
+}
---
+++ src/com/epam/module5/task02/Task02.java
@@ -0,0 +1,10 @@
+package com.epam.module5.task02;
+
+/**
+ * Created by pxjok on 26.10.2015.
+ */
+public class Task02 {
+    public static void main(String[] args) {
+
+    }
+}
